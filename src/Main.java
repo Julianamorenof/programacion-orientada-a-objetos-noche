c986bd4d9127c1ec912b95f92eb033f6b805bcf9
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-02-13 20:53:17 -0500

    salida de consola

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    IO.println(String.format("Hello and welcome!"));
+    System.out.println("Hola mundo");
+    System.out.println("Repaso comando");
+
 }

c4370430e429b9c60bf47f9baa1cc9e7e8b50d9f
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-02-17 12:44:26 -0500

    switch

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,7 @@ void main() {
     System.out.println("El lenguaje de programacion que visto y aprendido es c++");
     System.out.println("L solucion es arreglar la ruta");
     System.out.println("Nueva rama");
+    System.out.println("Tarea switch");
 
 }
+
